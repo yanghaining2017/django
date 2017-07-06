f4c808398df44a42616197c8c6c5ebd7754f5c1b
yanghaining2017 <yn411812835@qq.com>
2017-07-06 23:11:44 +0800

    登录

--- dailyfresh/ttsx_user/urls.py
+++ dailyfresh/ttsx_user/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from ttsx_user import views
+import views
 urlpatterns=[
     url(r"^register/$",views.register),
     url(r"^register_check/$",views.register_check),
     url(r"^register_valid/$",views.register_valid),
+    url(r"^login/$",views.login),
+    url(r"^login_handle/$",views.login_handle),
+    url(r"^user_center_info/$",views.user_center_info),
 ]
--- dailyfresh/ttsx_user/views.py
+++ dailyfresh/ttsx_user/views.py
@@ -6,7 +6,7 @@ from models import UserInfo
 # Create your views here.
 def register(request):
     #继承
-    context={'title':'注册'}
+    context={'title':'注册','top':'0'}
     return render(request,'user/register.html',context)
 
 def register_check(request):
@@ -33,6 +33,37 @@ def register_valid(request):
     data=UserInfo.objects.filter(uname=uname).count()
     context={'valid':data}
     return JsonResponse(context)
+
+def login(request):
+    context={'title':'登录','top':'0'}
+    return render(request,'user/login.html',context)
+
+def login_handle(request):
+    post=request.POST
+    uname=post.get('uname')
+    upwd=post.get('upwd')
+
+    s1=sha1()
+    s1.update(upwd)
+    upwd_sha1=s1.hexdigest()
+
+    context={'title':'登录','uname':uname,'upwd':upwd,'top':'0'}
+    result=UserInfo.objects.filter(uname=uname)
+    if len(result)==0:
+        context['error_name']='用户名错误'
+        return render(request,'user/login.html',context)
+    else:
+        if result[0].upwd==upwd_sha1:
+            response=redirect('/user/user_center_info.html')
+            request.session['uid']=result[0].id
+        else:
+            context['error_pwd']='密码错误'
+            return render(request,'user/login.html',context)
+
+def user_center_info(request):
+    user=UserInfo.objects.get(pk=request.session['uid'])
+    context={'user':user}
+    return render(request,'user/user_center_info.html',context)
 
 
 
