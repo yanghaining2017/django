acf3b576ea74c21f42b4f1866ae584a64ec47648
yanghaining2017 <yn411812835@qq.com>
2017-07-08 20:56:00 +0800

    分页未完

--- dailyfresh/ttsx_goods/urls.py
+++ dailyfresh/ttsx_goods/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 import views
 urlpatterns=[
 
-    #url(r'^index/$',views.index),
+    url(r'^index/$',views.index),
+    url(r'^list(\d+)/$',views.goods_list),
 
 ]
--- dailyfresh/ttsx_goods/views.py
+++ dailyfresh/ttsx_goods/views.py
@@ -1,7 +1,30 @@
+#coding=utf-8
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
-'''
-def ttsx_goods(request):
-    return render(request,'ttsx_goods/index.html')
-'''
+
+def index(request):
+    type_list=TypeInfo.objects.all()
+    list1=[]
+    for type1 in type_list:
+        new_list=type1.goodsinfo_set.order_by('-id')[0:4]
+        click_list=type1.goodsinfo_set.order_by('-gclick')[0:4]
+        list1.append({'new_list':new_list,'click_list':click_list,'t1':type1})
+    context={'list1':list1,'title':'首页'}
+    return render(request,'ttsx_goods/index.html',context)
+
+def goods_list(request):
+    t1=TypeInfo.objects.get(pk=int(tid))
+    new_list=t1.goodsinfo_set.order_by('-id')[0:2]
+    glist=t1.goodsinfo_set.order_by('-id')
+    context={'cart_show':1}
+    return render(request,'ttsx_goods/list.html',context)
+
+
+
+
+
+
+
+
+
